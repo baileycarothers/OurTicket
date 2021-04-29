9b7e69c2765ec2457f0292039a083ac53e47e346
HillbillyZT <chris30257@gmail.com>
2021-04-29 01:10:10 -0500

    101 Dalmations

--- Source/Unit_test.py
+++ Source/Unit_test.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#Unit Test 1
 
 import unittest
 from dbacc import db_ticket
@@ -14,7 +13,7 @@ class TestAccessMethods(unittest.TestCase):
         result = ticket.get_ticket_by_tid(tid)
         self.assertEqual(result, (tid, 0, 0, '1', '2', '3'))
     
-    
+    # Successful Test #3
     def test_upvote(self):
         ticket = db_ticket("OurTicket")
         tid = ticket.get_tid_by_name("1")
