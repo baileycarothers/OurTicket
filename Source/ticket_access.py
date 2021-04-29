067c223025d3d1729479e31931e796a1f3bb1358
HillbillyZT <chris30257@gmail.com>
2021-04-29 00:25:09 -0500

    Update ticket_access.py

--- Source/ticket_access.py
+++ Source/ticket_access.py
@@ -73,6 +73,7 @@ def main(argv):
        # tick = ticket.get_ticket_by_tid(f_tid)
         #for row in tick:
          #   print(row)
+        print("testingtop")
         if(f_vote):
             print("testing")
             ticket.upvote_ticket(f_tid)
