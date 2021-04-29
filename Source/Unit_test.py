098d5fbff71826c83bf47fdae380536f7f10c446
HillbillyZT <chris30257@gmail.com>
2021-04-29 01:09:35 -0500

    ayy 100

--- Source/Unit_test.py
+++ Source/Unit_test.py
@@ -12,9 +12,17 @@ class TestAccessMethods(unittest.TestCase):
         ticket.add_ticket(0, 0, "1", "2", "3")
         tid = ticket.get_tid_by_name("1")
         result = ticket.get_ticket_by_tid(tid)
-        print(result)
         self.assertEqual(result, (tid, 0, 0, '1', '2', '3'))
     
+    
+    def test_upvote(self):
+        ticket = db_ticket("OurTicket")
+        tid = ticket.get_tid_by_name("1")
+        ticket.upvote_ticket(tid)
+        result = ticket.get_ticket_by_tid(tid)
+        self.assertEqual(result, (tid, 0, 1, '1', '2', '3'))
+    
+    # Successful Test #3
     def test_remove(self):
         ticket = db_ticket("OurTicket")
         
@@ -22,6 +30,8 @@ class TestAccessMethods(unittest.TestCase):
         ticket.del_ticket(tid)
         result = ticket.get_ticket_by_tid(tid)
         self.assertEqual(result, False)
+        
+    
     
 if __name__ == '__main__':
     unittest.main()
