717af94ae6a987368630523dfd41446e78526636
HillbillyZT <chris30257@gmail.com>
2021-04-28 20:47:58 -0500

    Update read_posts.php

--- Source/Web/read_posts.php
+++ Source/Web/read_posts.php
@@ -1,7 +1,25 @@
 <html>
 <body>
 <?php 
-    echo "<h1>" . Big Nuts XD . "</h1>";
+    //echo "<h1>" . Big Nuts XD . "</h1>";
+    //Some command for getting the number of tickets from the python
+    
+    
+    //for loop for each ticket
+    $ticket_count = 10;
+    for($i = 0; $i < $ticket_count; $i++){
+        echo "Ticket No."; 
+        echo "Ticket Name:";
+        echo "Ticket Category:";
+        echo "Ticket Priority:";
+        echo "Ticket Description:";
+    }
+    
+    echo "Ticket No."; 
+    echo "Ticket Name:";
+    echo "Ticket Category:";
+    echo "Ticket Priority:";
+    echo "Ticket Description:";
 ?>
 </body>
 </html>
