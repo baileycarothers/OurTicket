27701fd430a99902a6b482664c0a41c2b264b472
HillbillyZT <chris30257@gmail.com>
2021-04-29 00:56:57 -0500

    jkl;jkl;

--- Source/Unit_test.py
+++ Source/Unit_test.py
@@ -1,1 +1,14 @@
 #Unit Test 1
+import unittest
+from dbacc import db_ticket
+
+class TestAccessMethods(unittest.TestCase):
+    def test_create_and_find(self):
+        ticket = db_ticket("OurTicket")
+        ticket.add_ticket(0, 0, "1", "2", "3")
+        result = ticket.get_ticket_by_tid(ticket.get_tid_by_name)
+        print(result)
+        
+if __name__ == '__main__':
+    unittest.main()
+        
