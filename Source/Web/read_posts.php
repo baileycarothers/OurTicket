5ce313c7a292d5fdbb1fab01c87db15c3e134538
HillbillyZT <chris30257@gmail.com>
2021-04-28 23:56:24 -0500

    Update read_posts.php

--- Source/Web/read_posts.php
+++ Source/Web/read_posts.php
@@ -41,7 +41,8 @@
     $close = $_POST['close'];
     $close_e = explode(",", $close);
     
-    #print($upvote[1] . )
+    print($upvote_e[1]);
+    print($upvote_e[0]);
     
     if($upvote){
         $out = shell_exec("./../ticket_access.py -f ".$upvote_e[1]." -v 2>&1");
