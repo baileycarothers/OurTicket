046e8670b3f6ef2447e47f6b00ff4672e5d7d665
HillbillyZT <chris30257@gmail.com>
2021-04-29 01:28:01 -0500

    Update Unit_test.py

--- Source/Unit_test.py
+++ Source/Unit_test.py
@@ -5,6 +5,15 @@ from dbacc import db_ticket
 
 class TestAccessMethods(unittest.TestCase):
     
+    # Successful Test #2
+    def test_upvote(self):
+        ticket = db_ticket("OurTicket")
+        tid = ticket.get_tid_by_name("1")
+        ticket.upvote_ticket(tid)
+        result = ticket.get_ticket_by_tid(tid)
+        self.assertEqual(result, (tid, 0, 1, '1', '2', '3'))
+    
+    
     # Successful Test #1
     def test_create_and_find(self):
         ticket = db_ticket("OurTicket")
@@ -13,13 +22,6 @@ class TestAccessMethods(unittest.TestCase):
         result = ticket.get_ticket_by_tid(tid)
         self.assertEqual(result, (tid, 0, 0, '1', '2', '3'))
     
-    # Successful Test #3
-    def test_upvote(self):
-        ticket = db_ticket("OurTicket")
-        tid = ticket.get_tid_by_name("1")
-        ticket.upvote_ticket(tid)
-        result = ticket.get_ticket_by_tid(tid)
-        self.assertEqual(result, (tid, 0, 1, '1', '2', '3'))
     
     # Successful Test #3
     def test_remove(self):
