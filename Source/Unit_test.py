268e97cf01be7db41edc5e6d184f1359f124ef59
HillbillyZT <chris30257@gmail.com>
2021-04-29 01:01:13 -0500

    Update Unit_test.py

--- Source/Unit_test.py
+++ Source/Unit_test.py
@@ -8,7 +8,7 @@ class TestAccessMethods(unittest.TestCase):
     def test_create_and_find(self):
         ticket = db_ticket("OurTicket")
         ticket.add_ticket(0, 0, "1", "2", "3")
-        result = ticket.get_ticket_by_tid(ticket.get_tid_by_name)
+        result = ticket.get_ticket_by_tid(ticket.get_tid_by_name("1"))
         print(result)
         
 if __name__ == '__main__':
