8dc6ad9157d4514bdbab24f3faaf44e32b81883b
HillbillyZT <chris30257@gmail.com>
2021-04-29 01:02:46 -0500

    Update Unit_test.py

--- Source/Unit_test.py
+++ Source/Unit_test.py
@@ -8,8 +8,10 @@ class TestAccessMethods(unittest.TestCase):
     def test_create_and_find(self):
         ticket = db_ticket("OurTicket")
         ticket.add_ticket(0, 0, "1", "2", "3")
-        result = ticket.get_ticket_by_tid(ticket.get_tid_by_name("1"))
+        tid = ticket.get_tid_by_name("1")
+        result = ticket.get_ticket_by_tid(tid)
         print(result)
+        self.assertEqual(result, (tid, 0, 0, '1', '2', '3')))
         
 if __name__ == '__main__':
     unittest.main()
