f0db373399e75c8622dff13dddc9268be2d80a13
HillbillyZT <chris30257@gmail.com>
2021-04-28 22:09:10 -0500

    Update submitted.php

--- Source/Web/submitted.php
+++ Source/Web/submitted.php
@@ -5,7 +5,7 @@ $category = $_POST['category'];
 $priority = $_POST['priority'];
 $details = $_POST['details'];
 
-$toss = shell_exec("./../ticket_access.py -n \"". $title . "\" -c \"" . $category . "\" -d \"" . $details . "\"");
+$toss = shell_exec("./../ticket_access.py -a -n \"". $title . "\" -c \"" . $category . "\" -d \"" . $details . "\"");
 
 echo "<h3>Submission Received, code: " . $toss;
 ?>
